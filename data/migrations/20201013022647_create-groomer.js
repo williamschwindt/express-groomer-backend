04090720f79887507c1c427d43ca238eb943705c
Luis Abellan <luis.abellan.dev@gmail.com>
2020-10-13 07:52:27 +0200

    added zip to groomer table
    not yet in main branch so I edited migration file

--- data/migrations/20201013022647_create-groomer.js
+++ data/migrations/20201013022647_create-groomer.js
@@ -6,6 +6,7 @@ exports.up = (knex) => {
       table.string('name').notNullable();
       table.string('lastname').notNullable();
       table.string('address').notNullable();
+      table.string('zip');
       table.integer('phone').notNullable().unique();
       table.string('email').notNullable().unique();
       table.string('city');
