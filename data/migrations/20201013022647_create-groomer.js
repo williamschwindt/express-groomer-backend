119e9cc7b163313eabe3b8115d173d5731590bb1
Luis Abellan <luis.abellan.dev@gmail.com>
2020-10-13 07:50:53 +0200

    groomer table created, .gitignore updated

---
+++ data/migrations/20201013022647_create-groomer.js
@@ -0,0 +1,23 @@
+exports.up = (knex) => {
+  return knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('Groomer', function (table) {
+      table.string('id').notNullable().unique().primary();
+      table.string('name').notNullable();
+      table.string('lastname').notNullable();
+      table.string('address').notNullable();
+      table.integer('phone').notNullable().unique();
+      table.string('email').notNullable().unique();
+      table.string('city');
+      table.string('state');
+      table.string('country');
+      table.string('photo_url').unique();
+      table.integer('walk_rate');
+      table.integer('day_care_rate');
+      table.timestamps(true, true);
+    });
+};
+
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExists('Groomer');
+};
---
+++ data/migrations/20201013074025_customer.js
@@ -0,0 +1,22 @@
+exports.up = (knex) => {
+  return knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('Customer', function (table) {
+      table.string('id').notNullable().unique().primary();
+      table.string('name').notNullable();
+      table.string('lastname').notNullable();
+      table.string('address').notNullable();
+      table.string('zip');
+      table.integer('phone').notNullable().unique();
+      table.string('email').notNullable().unique();
+      table.string('city');
+      table.string('state');
+      table.string('country');
+      table.string('photo_url').unique();
+      table.timestamps(true, true);
+    });
+};
+
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExists('Customer');
+};
