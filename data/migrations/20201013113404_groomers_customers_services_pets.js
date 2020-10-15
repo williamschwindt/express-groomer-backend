1e9281d3e430240526aca74c9b44b4c4e4d3b410
Luis Abellan <luis.abellan.dev@gmail.com>
2020-10-15 15:29:39 +0200

    make pet_id, groomer_id and service_id notNullable()

--- data/migrations/20201013113404_groomers_customers_services_pets.js
+++ data/migrations/20201013113404_groomers_customers_services_pets.js
@@ -4,17 +4,9 @@ exports.up = (knex) => {
     .createTable('groomers_customers_services_pets', function (table) {
       table.string('id').notNullable().unique();
       table.string('customer_id').references('id').inTable('customers');
-      table.string('pet_id').notNullable().references('id').inTable('pets');
-      table
-        .string('groomer_id')
-        .notNullable()
-        .references('id')
-        .inTable('groomers');
-      table
-        .string('service_id')
-        .notNullable()
-        .references('id')
-        .inTable('services');
+      table.string('pet_id').references('id').inTable('pets');
+      table.string('groomer_id').references('id').inTable('groomers');
+      table.string('service_id').references('id').inTable('services');
 
       table.primary(['customer_id', 'pet_id', 'groomer_id', 'service_id']);
       table.timestamps(true, true);
--- data/seeds/006-groomers_customers_services_pets.js
+++ data/seeds/006-groomers_customers_services_pets.js
@@ -6,6 +6,38 @@ exports.seed = function (knex) {
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex('groomers_customers_services_pets').insert();
+      return knex('groomers_customers_services_pets').insert([
+        // {
+        //   id: '1',
+        //   customer_id: '2',
+        //   pet_id: '2',
+        //   groomer_id: '1',
+        //   service_id: '2',
+        // },
+      ]);
     });
 };
+
+// const faker = require('faker/locale/en_US');
+
+// const faker = require('faker');
+
+// const groomers_customers_services_pets = [...new Array(2)].map((i, idx) => ({
+//   id: faker.random.number(),
+//   customer_id: '2',
+//   pet_id: '2',
+//   groomer_id: '1',
+//   service_id: '2',
+// }));
+
+// exports.seed = function (knex) {
+//   // Deletes ALL existing entries
+//   return knex('groomers_customers_services_pets')
+//     .del()
+//     .then(function () {
+//       // Inserts seed entries
+//       return knex('groomers_customers_services_pets').insert(
+//         groomers_customers_services_pets
+//       );
+//     });
+// };
