327504136388b0b92d4ee4860c114b062b4a46d4
William Schwindt <williammschwindt@gmail.com>
2020-10-17 17:34:55 -0700

    skeleton routes created in customer router

--- api/customers/customersRouter.js
+++ api/customers/customersRouter.js
@@ -1,4 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  try {
+    console.log('');
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    console.log('');
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    console.log('');
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post('/:id', async (req, res) => {
+  try {
+    console.log('');
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    console.log('');
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
