111cdaf3b0c38164a2222224105ba466933b494f
William Schwindt <williammschwindt@gmail.com>
2020-11-23 12:19:53 -0800

    updated knexfile paths

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,8 @@ module.exports = {
   development: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    migrations: { directory: '../data/migrations' },
-    seeds: { directory: '../data/seeds' },
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
     pool: {
       min: 2,
       max: 10,
@@ -16,14 +16,14 @@ module.exports = {
   test: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    migrations: { directory: '../data/migrations' },
-    seeds: { directory: '../data/seeds' },
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
   },
 
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    migrations: { directory: '../data/migrations' },
-    seeds: { directory: '../data/seeds' },
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
   },
 };
