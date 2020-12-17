e76be301d33f0161196dab0977f662133d3c8547
William Schwindt <williammschwindt@gmail.com>
2020-12-17 15:55:31 -0800

    clean up

--- server.js
+++ server.js
@@ -1,31 +1,28 @@
-// const app = require('./api/app.js');
 const express = require('express');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
-// const indexRouter = require('./index/indexRouter');
-// const profileRouter = require('./profile/profileRouter');
-const customerRouter = require('./api/customers/customersRouter');
-const groomerRouter = require('./api/groomers/groomersRouter');
-const app = express();
+const customerRouter = require('./routers/customers/customersRouter');
+const groomersRouter = require('./routers/groomers/groomersRouter');
+const server = express();
+const port = process.env.PORT || 6000;
 
-app.use(express.json());
-app.use(helmet());
-app.use(cors());
-app.use(cookieParser());
-// application routes
-// app.use('/', indexRouter);
-// app.use(['/profile', '/profiles'], profileRouter);
-app.use('/customers', customerRouter);
-app.use('/groomers', groomerRouter);
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+server.use('/customers', customerRouter);
+server.use('/groomers', groomersRouter);
 
-app.use((err, req, res) => {
+server.use((err, req, res) => {
   console.log(err);
   res.status(500).json({
     message: 'somthing went wrong',
   });
 });
 
-const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
-// app.timeout = 60 * 10 * 1000;
+if (!module.parent) {
+  server.listen(port, () => {
+    console.log(`server running on port: ${port}`);
+  });
+}
+
+module.exports = server;
